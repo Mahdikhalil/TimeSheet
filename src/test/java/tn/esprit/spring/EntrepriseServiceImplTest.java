05a225be599ef17e1a472917e015a9dc25ecb2ed
dorra <dorra.kerro@gmail.com>
2021-10-12 23:29:15 +0100

    test

--- src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
@@ -7,4 +7,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EntrepriseServiceImplTest {
+
+
+    @Autowired
+    IEntrepriseService es ;
+    @Autowired
+    EntrepriseRepository entrepriseRepoistory;
+    @Autowired
+    DepartementRepository deptRepoistory;
+
+/*
+    private static final Logger l = LogManager.getLogger(TimesheetServiceImplTest.class);
+
+    @Test
+    public void testAjouterEntreprise() {
+
+        int entreprise = es.ajouterEntreprise(new Entreprise("entreprise1","entreprise2"));
+        assertNotNull(id);
+        l.info("entreprise ajouté " + entreprise);
+
+    }
+
+    @Test
+    public void testAjouterDepartement(){
+
+        int dept = es.ajouterDepartement(new Departement("dept1","dept2"));
+        assertNotNull(id);
+        l.info("departement ajouté " + dept);
+    }
+    */
 }
