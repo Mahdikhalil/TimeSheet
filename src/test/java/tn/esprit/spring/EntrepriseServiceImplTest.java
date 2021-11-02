ed022874c5284beb6d408e0268777caa4061fec2
Emna TIRA <emnatira97@gmail.com>
2021-11-03 00:52:13 +0100

    Sonar!

--- src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
@@ -8,36 +8,8 @@ import tn.esprit.spring.repository.DepartementRepository;
 import tn.esprit.spring.repository.EntrepriseRepository;
 import tn.esprit.spring.services.IEntrepriseService;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 public class EntrepriseServiceImplTest {
 
 
-    @Autowired
-    IEntrepriseService es ;
-    @Autowired
-    EntrepriseRepository entrepriseRepoistory;
-    @Autowired
-    DepartementRepository deptRepoistory;
-
-/*
-    private static final Logger l = LogManager.getLogger(TimesheetServiceImplTest.class);
-
-    @Test
-    public void testAjouterEntreprise() {
-
-        int entreprise = es.ajouterEntreprise(new Entreprise("entreprise1","entreprise2"));
-        assertNotNull(id);
-        l.info("entreprise ajouté " + entreprise);
-
-    }
-
-    @Test
-    public void testAjouterDepartement(){
-
-        int dept = es.ajouterDepartement(new Departement("dept1","dept2"));
-        assertNotNull(id);
-        l.info("departement ajouté " + dept);
-    }
-    */
 }
