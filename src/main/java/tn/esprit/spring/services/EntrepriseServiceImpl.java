231a6e332f83fade1a0eb610f599a902376ad67c
dorra <dorra.kerro@gmail.com>
2021-11-06 13:07:45 +0100

    jenkins try 2

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -66,6 +66,7 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	}
 
 
+
 	@Transactional
 	public Entreprise deleteEntrepriseById(int entrepriseId) {
 	Entreprise entreprise=entrepriseRepoistory.findById(entrepriseId).get();
